51c9fbf606c10cef9528251bb202215e280c9936
HaroldHoang <viethung18tuoi@gmail.com>
2024-04-22 10:49:03 +0700

    init project

---
+++ src/main/java/com/example/oshaneat/OshanEatApplication.java
@@ -0,0 +1,13 @@
+package com.example.oshaneat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OshanEatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OshanEatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/oshaneat/OshanEatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oshaneat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OshanEatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
